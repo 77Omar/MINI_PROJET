a332e101be967c22251a02ea8c1a2779f631ed5b
Omar <fayeomzolive@gmail.com>
2020-04-10 14:57:58 +0000

    je suis Omar faye

---
+++ pageconnexion.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet"  type="text/css" href="pageconnexion.css">
+    <title>Page de connexion</title>
+</head>
+<body>
+<div class="general">
+    <div class="one">
+        <div id="logo">
+         <img src="images/logo-QuizzSA.png" alt="">
+        </div>
+       <h2>Le plaisir de jouer</h2>
+    </div>
+
+<div class="forme">
+<div class="bloc">
+
+    <div class="two">
+     <h3>Login Form</h3>
+    
+    </div>
+   <form action="" method="post">
+   <p><input type="text" name="login" placeholder="    login" class="login"></p>
+   <p><input type="text" name="password" placeholder="    password" class="password"></p>
+   <p><input type="submit" value="connexion" placeholder="     |  connexion" class="connexion" name="btn"></p>
+   </form>
+   <p><link rel="stylesheet" href="#"><h5>S'inscrire pour jouer?</h5></p>
+  </div>
+ </div>
+</div>
+</body>
+</html>
