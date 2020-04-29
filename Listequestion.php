723737fce00a7ee42fb78a011cfe4b5f23ec22fd
Omar <fayeomzolive@gmail.com>
2020-04-29 15:05:32 +0000

    Je suis Omar faye

--- Listequestion.php
+++ Listequestion.php
@@ -24,56 +24,61 @@ $Tab_Json=json_decode(file_get_contents("question.json"),true);
     </div>
   <div class="right">
   <?php
-
-   $Nbre_de_question=5;
   if(isset($_POST['btn'])){
     $Nbre_de_question=$_POST['nbre_question'];
     if(empty($Nbre_de_question)){
         echo "Veuillez entre un nombre svp!";
+    }elseif($Nbre_de_question<=0){
+      echo "veuillez entrez un nombre positif svp!";
     }
-  else{
-        $nbr_elements=count($Tab);
-        $nbr_par_page=$Nbre_de_question; 
-        $nbr_de_page=ceil($nbr_elements/$nbr_par_page);
-         
-       if(isset($_GET['pages'])){
-         $numberpage=$_GET['pages'];
-       }else{
-         $numberpage=1;
+     else{
+         $_SESSION['Nbre']=$Nbre_de_question;
+         }
+    }
+    if(!empty($_SESSION['Nbre'])){
+      $nbr_elements=count($Tab);
+      $nbr_par_page=$_SESSION['Nbre']; 
+      $nbr_de_page=ceil($nbr_elements/$nbr_par_page);
+       
+     if(isset($_GET['pages'])){
+       $numberpage=$_GET['pages'];
+     }else{
+       $numberpage=1;
+     }
+      echo "<p>le nombre de page est:".$nbr_de_page.'</p>';
+      $IndiceDebut=($numberpage-1) * $nbr_par_page;
+      $IndiceFin=$IndiceDebut+$nbr_par_page-1;
+     
+      for($i=$IndiceDebut; $i<=$IndiceFin; $i++){
+         if(array_key_exists($i,$Tab)){
+             echo ($i+1).".".$Tab[$i]["questions"]."<br/></br>";
+             foreach($Tab[$i]['reponse'] as $value){
+                 if($Tab[$i]['type_reponse']=="Texte"){
+                     echo "<input type='text' name=''>";
+                     echo"<br/>";
+                 }elseif($Tab[$i]['type_reponse']=="choix_multiple"){
+                     echo "<input type='checkbox' name=''>".$value;
+                     echo"<br/>";
+                 }else{
+                     echo "<input type='radio' name=''>".$value;
+                     echo"<br/>";
+                 }
+             }
+             echo "<hr>";
+         }
        }
-        echo "<p>le nombre de page est:".$nbr_de_page.'</p>';
-        $IndiceDebut=($numberpage-1) * $nbr_par_page;
-        $IndiceFin=$IndiceDebut+$nbr_par_page-1;
-       
-        for($i=$IndiceDebut; $i<=$IndiceFin; $i++){
-           if(array_key_exists($i,$Tab)){
-               echo ($i+1).".".$Tab[$i]["questions"]."<br/></br>";
-               foreach($Tab[$i]['reponse'] as $value){
-                   if($Tab[$i]['type_reponse']=="Texte"){
-                       echo "<input type='text' name=''>";
-                       echo"<br/>";
-                   }elseif($Tab[$i]['type_reponse']=="choix_multiple"){
-                       echo "<input type='checkbox' name=''>".$value;
-                       echo"<br/>";
-                   }else{
-                       echo "<input type='radio' name=''>".$value;
-                       echo"<br/>";
-                   }
-               }
-               echo "<hr>";
-           }
-         }
-       
-         if($numberpage > 1){
-           $precedent = $numberpage-1;
-           echo '</br><a class="precedent" href="creationpageadmin.php?page=Listequestion&pages='.$precedent.' ">Precedent</a> &nbsp;';
-         }
-         if($numberpage < $nbr_de_page){
-           $suivant= $numberpage+1;
-           echo '<a class="suivant" href="creationpageadmin.php?page=Listequestion&pages='.$suivant.' ">Suivant</a> &nbsp;';
-         }
+     
+       if($numberpage > 1){
+         $precedent = $numberpage-1;
+         echo '</br><a class="precedent" href="creationpageadmin.php?page=Listequestion&pages='.$precedent.' ">Precedent</a> &nbsp;';
+       }
+       if($numberpage < $nbr_de_page){
+         $suivant= $numberpage+1;
+         echo '<a class="suivant" href="creationpageadmin.php?page=Listequestion&pages='.$suivant.' ">Suivant</a> &nbsp;';
+       }
+
     }
-  }
+        
 
   ?>
 
